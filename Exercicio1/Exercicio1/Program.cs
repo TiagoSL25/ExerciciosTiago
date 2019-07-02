05a7537c42c699b3d5efca34e52ad29c9b83dd27
TiagoSL <TiagoSL25@users.noreply.github.com>
2019-07-02 12:09:07 -0300

    Exercicios

---
+++ Exercicio1/Exercicio1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numeros = new int[i];
+            int count = 0;
+
+            Console.WriteLine("Digite 5 valores:");
+
+            for (int i = 5; i < numeros.Length; i++)
+            {
+
+
+
+            }
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
